4151ec0b7790208a32677e029c52b9c4e532bead
Anton Makiievskyi <iammckiev@gmail.com>
2019-12-03 17:25:51 +0700

    subfolder add

--- hw3/model_sweep.py
+++ hw3/model_sweep.py
@@ -1,4 +1,4 @@
 import subprocess
 
 for model in ['s1', 's2', 's3', 'def256', 'def128', 'def']:
-    subprocess.run('python3 run_dqn_atari.py --model %s --doubleQ'%model, shell = True)
+    subprocess.run('python3 run_dqn_atari.py --model %s --doubleQ --subdir ModelSweep'%model, shell = True)
